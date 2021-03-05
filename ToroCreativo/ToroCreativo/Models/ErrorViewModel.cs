0e8e7824ca5a087067d3ef3cf06bc8b61e78d8fc
Juan Rivera <juanriverarestrepo0103@gmail.com>
2021-03-04 22:05:38 -0500

    Se agrego estructura del proyecto

---
+++ ToroCreativo/ToroCreativo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ToroCreativo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
