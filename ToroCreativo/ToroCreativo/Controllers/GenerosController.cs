453cc1794f5caccf2a5ee25f44f927b4424ded13
Santiago419 <santiagomolinaarboleda@gmail.com>
2021-03-15 22:23:11 -0500

    Se creo Crud generos

---
+++ ToroCreativo/ToroCreativo/Controllers/GenerosController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToroCreativo.Models.DAL;
+using ToroCreativo.Models.Entities;
+
+namespace ToroCreativo.Controllers
+{
+    public class GenerosController : Controller
+    {
+        private readonly DbContextToroCreativo _context;
+
+        public GenerosController(DbContextToroCreativo context)
+        {
+            _context = context;
+        }
+
+        // GET: Generos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.generos.ToListAsync());
+        }
+
+        // GET: Generos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var generos = await _context.generos
+                .FirstOrDefaultAsync(m => m.Idgenero == id);
+            if (generos == null)
+            {
+                return NotFound();
+            }
+
+            return View(generos);
+        }
+
+        // GET: Generos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Generos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Idgenero,Nombre,Estado")] Generos generos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(generos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(generos);
+        }
+
+        // GET: Generos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var generos = await _context.generos.FindAsync(id);
+            if (generos == null)
+            {
+                return NotFound();
+            }
+            return View(generos);
+        }
+
+        // POST: Generos/Edit/5
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Idgenero,Nombre,Estado")] Generos generos)
+        {
+            if (id != generos.Idgenero)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(generos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GenerosExists(generos.Idgenero))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(generos);
+        }
+
+        private bool GenerosExists(int id)
+        {
+            return _context.generos.Any(e => e.Idgenero == id);
+        }
+
+        public async Task<IActionResult> CambiarEstado(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var generos = await _context.generos
+                .FirstOrDefaultAsync(m => m.Idgenero == id);
+
+            if (generos == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                if (generos.Estado.Equals("Habilitado"))
+                    generos.Estado = "Deshabilitado";
+                else
+                    generos.Estado = "Habilitado";
+
+                _context.generos.Update(generos);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw new Exception();
+            }
+
+            return RedirectToAction("Index") ;
+        }
+    }
+}
--- ToroCreativo/ToroCreativo/Models/DAL/DbContextToroCreativo.cs
+++ ToroCreativo/ToroCreativo/Models/DAL/DbContextToroCreativo.cs
@@ -14,5 +14,7 @@ namespace ToroCreativo.Models.DAL
 
         }
         public DbSet<Categorias> categorias { get; set; }
+        public DbSet<Generos> generos { get; set; }
+
     }
 }
